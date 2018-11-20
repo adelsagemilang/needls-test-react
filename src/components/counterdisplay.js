17f40a886fd5f9f2030a1f9e31f0153d19752444
Adelsa Gemilang Fadila <adelsagemilangfadila@gmail.com>
2018-11-20 22:28:40 +0700

    updating comments

--- src/components/counterdisplay.js
+++ src/components/counterdisplay.js
@@ -6,10 +6,6 @@ import '../index.css';
   concern is displaying the values it recieves from its parent container component (or smart component). 
 
   We should also use propTypes or flow to add type constraints to the incoming data.
-
-  Also, we use React.PureComponent in this case because this is a "pure component" and can be speed up
-  by letting react now that this is a simple component with No life-cycle callback hooks, business logic, data fetching or 
-  any other costly operations. 
 */
 export default class CounterDisplay extends Component {
   render() {
